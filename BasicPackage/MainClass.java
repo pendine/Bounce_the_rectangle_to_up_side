769400e1da66cb0bc64eb47868e3c3b845619788
pendine <neokin7734@gmail.com>
2019-05-02 02:12:00 +0900

    Create MainClass.java

---
+++ BasicPackage/MainClass.java
@@ -0,0 +1,9 @@
+package BasicPackage;
+
+public class MainClass {
+    public static void main(String args[]){
+        DroppedBlock dropBlock = new DroppedBlock();
+        FrameTool ft = new FrameTool(dropBlock);
+
+    }
+}
